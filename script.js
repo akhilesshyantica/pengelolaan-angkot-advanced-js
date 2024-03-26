b4c290ea900a9a1b99cbf2e7b7f67430fd9902d9
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-03-26 20:08:17 +0700

    membuat method penumpangNaik

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ function Angkot (sopir, trayek, penumpang, kas){
     this.trayek = trayek;
     this.penumpang = penumpang;
     this.kas = kas;
+
+    // kita buat method
+    this.penumpangNaik = function(namaPenumpang){
+        this.penumpang.push(namaPenumpang);
+        // this.penumpang diatas diambil dari properti array penumpang diatas.
+        return this.penumpang;
+    }
 }
 
 // inisialisasi dari object
 var angkot1 = new Angkot("Akhiles Aga", ["Kedungwuluh", "Karangklesem"], [], 0);
+
+var angkot2 = new Angkot ("Leo Messi", ["Purwokerto", "Bumiayu"] , [], 0);
