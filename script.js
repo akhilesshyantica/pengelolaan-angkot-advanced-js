3705f37f652e8eab3b1be0d12f7b12f2630622ff
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-03-26 20:18:37 +0700

    membuat method untuk penumpang turun dan membayar

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ function Angkot (sopir, trayek, penumpang, kas){
         // this.penumpang diatas diambil dari properti array penumpang diatas.
         return this.penumpang;
     }
+
+
+    // method untuk penumpang turun
+    this.penumpangTurun = function(namaPenumpang, bayar){
+        if(this.penumpang.length === 0){
+            alert("Angkot masih kosong!");
+        }
+        for(var i = 0; i < this.penumpang.length; i++){
+            if(this.penumpang[i] == namaPenumpang){
+                this.penumpang[i] = undefined;
+                this.kas += bayar;
+                return this.penumpang;
+            }
+        }
+    }
 }
 
 // inisialisasi dari object
